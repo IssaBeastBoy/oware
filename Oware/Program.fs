0dff0275bd29189b89d27e294342c1adbf80f409
g16t5351 <g16t5351@HUG62>
2019-03-21 21:51:41 +0200

    Only 4 tests remain

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,5 +1,7 @@
 ﻿module Oware
 
+open System.Threading.Tasks.Dataflow
+
 type StartingPosition =
     | South
     | North
@@ -51,65 +53,84 @@ let amendedBoard pos board =
         | 12 -> a,b,c,d,e,f,g,h,i,j,k,0
     {board with houses=newBoard}
 
+let updateScoreBoard n board player =
+    let {score=(p1,p2)} = board
+    let totalScore =
+        match player with 
+        | South -> (p1+n),p2
+        | North -> p1,(p2+n)
+    let newP = {board with score = totalScore}
+    match player with 
+    | South -> {newP with CurrentPlayer=North}
+    | North -> {newP with CurrentPlayer=South}
+    
+let scoreMethod board inputHouseNumber=
+    let {CurrentPlayer=player} = board
+    let rec innerScoreMethod currboard houseNumber score stopCase2= 
+        match stopCase2=0 with
+        | true -> updateScoreBoard score currboard player
+        | false ->
+            match player=(playerhouses houseNumber) with
+            | false -> updateScoreBoard score currboard player
+            | true ->
+                let actualHouseNumber =
+                    match houseNumber=0 with
+                    | true -> 1
+                    |false -> houseNumber
+                match getSeeds actualHouseNumber board with
+                | 3 -> innerScoreMethod (amendedBoard actualHouseNumber currboard) (actualHouseNumber-1) (score+3) (stopCase2-1) 
+                | 2 -> innerScoreMethod (amendedBoard actualHouseNumber currboard) (actualHouseNumber-1) (score+2) (stopCase2-1)
+                | _ -> updateScoreBoard score currboard player
+    innerScoreMethod board (inputHouseNumber) 0 2
+
 let setSeeds pos board=
     let {CurrentPlayer=player} = board
-    let houseNum = getSeeds pos board
+    let numberOfSeeds = getSeeds pos board
     let {houses=AmendedBoard} = amendedBoard pos board
-    let rec move housePosition stopCase accBoard =
-        let amendhousePosition = 
-            match housePosition<13 with
-            | false -> 1
-            | true -> housePosition
-
-        match stopCase=0 with
-        | true -> 
-        //check who is playing 
-        //Are they in the right range
-        // Get the values( check 3 back) then update score 
-            let newBoard = {board with houses=accBoard}
-            let {score=(p1,p2)} = board
-            let playerpoint = 
-                match player with
-                | South -> p1
-                | North -> p2
-            let rec scoreMethod currboard houseNumber score stopCase= 
-                let newPsps =
-                    let totalScore =
-                        match player with 
-                        | South -> (p1+score),p2
-                        | North -> p1,(p2+score)
-                    let newP = {currboard with score = totalScore}
-                    match player with 
-                    | South -> {newP with CurrentPlayer=North}
-                    | North -> {newP with CurrentPlayer=South}
+    match player=( playerhouses pos) with
+    | true -> board
+    | false ->
+        match numberOfSeeds=0 with
+        | true -> board
+        | false ->
+            let rec move housePosition stopCase accBoard =
+                let amendhousePosition = 
+                    match housePosition<13, stopCase=0 with
+                    | false,false -> 1
+                    | true,false -> housePosition
+                    | _ ->  housePosition-1
 
                 match stopCase=0 with
-                | true -> newPsps
-                | false ->
-                    match player=(playerhouses houseNumber), (getSeeds houseNumber newBoard)=3, (getSeeds houseNumber newBoard)=2 with
-                    | true,true, false ->scoreMethod (amendedBoard houseNumber currboard) (houseNumber-1) (playerpoint+3) (stopCase-1) //still need to find a way to determine who is playing
-                    | true,false,true-> scoreMethod (amendedBoard houseNumber currboard) (houseNumber-1) (playerpoint+2) (stopCase-1)
-                    | true,false,false -> newPsps
-                    | false,_,_ -> newPsps
-            scoreMethod newBoard amendhousePosition 0 2
-        | false -> 
-            let a,b,c,d,e,f,g,h,i,j,k,l=accBoard
-            let amendHouse = 
-                match amendhousePosition with
-                | 1 -> a+1,b,c,d,e,f,g,h,i,j,k,l
-                | 2 -> a,b+1,c,d,e,f,g,h,i,j,k,l
-                | 3 -> a,b,c+1,d,e,f,g,h,i,j,k,l
-                | 4 -> a,b,c,d+1,e,f,g,h,i,j,k,l
-                | 5 -> a,b,c,d,e+1,f,g,h,i,j,k,l
-                | 6 -> a,b,c,d,e,f+1,g,h,i,j,k,l
-                | 7 -> a,b,c,d,e,f,g+1,h,i,j,k,l 
-                | 8 -> a,b,c,d,e,f,g,h+1,i,j,k,l
-                | 9 -> a,b,c,d,e,f,g,h,i+1,j,k,l
-                | 10 -> a,b,c,d,e,f,g,h,i,j+1,k,l
-                | 11 -> a,b,c,d,e,f,g,h,i,j,k+1,l
-                | 12 -> a,b,c,d,e,f,g,h,i,j,k,l+1
-            move (amendhousePosition+1) (stopCase-1) amendHouse
-    move (pos+1) houseNum AmendedBoard
+                | true -> 
+                //check who is playing 
+                //Are they in the right range
+                // Get the values( check 3 back) then update score 
+                    let newBoard = {board with houses=accBoard}
+                    scoreMethod newBoard amendhousePosition
+
+                | false -> 
+                    let a,b,c,d,e,f,g,h,i,j,k,l=accBoard
+                    let newAmendedHousePosition =
+                        match pos=amendhousePosition with
+                        | true -> amendhousePosition+1
+                        |false -> amendhousePosition
+                    let amendHouse = 
+                        match newAmendedHousePosition with
+                        | 1 -> a+1,b,c,d,e,f,g,h,i,j,k,l
+                        | 2 -> a,b+1,c,d,e,f,g,h,i,j,k,l
+                        | 3 -> a,b,c+1,d,e,f,g,h,i,j,k,l
+                        | 4 -> a,b,c,d+1,e,f,g,h,i,j,k,l
+                        | 5 -> a,b,c,d,e+1,f,g,h,i,j,k,l
+                        | 5 -> a,b,c,d,e+1,f,g,h,i,j,k,l
+                        | 6 -> a,b,c,d,e,f+1,g,h,i,j,k,l
+                        | 7 -> a,b,c,d,e,f,g+1,h,i,j,k,l 
+                        | 8 -> a,b,c,d,e,f,g,h+1,i,j,k,l
+                        | 9 -> a,b,c,d,e,f,g,h,i+1,j,k,l
+                        | 10 -> a,b,c,d,e,f,g,h,i,j+1,k,l
+                        | 11 -> a,b,c,d,e,f,g,h,i,j,k+1,l
+                        | 12 -> a,b,c,d,e,f,g,h,i,j,k,l+1
+                    move (newAmendedHousePosition+1) (stopCase-1) amendHouse
+            move (pos+1) numberOfSeeds AmendedBoard
 
     
 let useHouse n board = 
@@ -128,9 +149,19 @@ let score board =
 // board -> string
 let gameState board =
     let {CurrentPlayer=(Playing)} =board
-    match Playing with
-    |South -> "South's turn"
-    |North -> "North's turn"
+    let {score =(playerOne,playerTwo)}=board
+    match (playerOne=24 && playerTwo=24) with
+    | true -> "Game ended in a draw"
+    | false -> 
+        match (playerTwo>=25) with
+        | true -> "North won"
+        | false -> 
+            match (playerOne>=25) with 
+            | true -> "South won"
+            | false -> 
+                match Playing with
+                |South -> "South's turn"
+                |North -> "North's turn"
     
 
 [<EntryPoint>]
